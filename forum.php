8a97e88f9c21a317b4a42230e332a6ec43658587
baganonline <74863308+baganonline@users.noreply.github.com>
2024-01-03 15:13:24 +0700

    Add files via upload

---
+++ forum.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Баган.Форум - форум и доска объявлений №1 в районе</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" href="images/logo.png" type="image/png">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<style>
+			.ifr{
+				border: 0px none; margin-left: -50px; height: 700px; margin-top: 0px; width: 1024px; overflow-x:auto;
+			}
+			.dv{
+				overflow: hidden; margin: 15px auto;width: 100%;position:relative;
+			}
+			@media screen and (max-width: 960px) {
+
+				.ifr{
+				border: 0px none; margin-left: 0px; margin-bottom: -60px;height: 600px; margin-top: 0px; width: 125%; overflow-x:auto;
+			}
+			.dv{
+				overflow: hidden;width: 100%;position:relative;
+			}
+
+}
+		</style>
+	</head>
+	<body class="right-sidebar is-preload">
+		<div id="page-wrapper">
+
+			<!-- Шапка -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1><a href="index.html" id="logo">Форум и ДО</a></h1>
+							</header>
+						</div>
+
+					<!-- Меню -->
+					<nav id="nav">
+						<ul>
+							<li style="white-space: nowrap;"><a href="index.html">Начало</a></li>
+							<li style="white-space: nowrap;"><a href="media.html">Баган.Медиа</a></li>
+							<li style="white-space: nowrap;"><a href="forum.php">Форум и ДО</a></li>
+							<li style="white-space: nowrap;"><a href="http://award.yzz.me/login/">Войти</a></li>
+							<li style="white-space: nowrap;"><a href="sprav.html">Баган.Справочник</a></li>
+							<li style="white-space: nowrap;"><a href="development.html">Разработчики</a></li>
+							<li style="white-space: nowrap;"><a href="/apk/">Мобильное приложение</a></li>
+						</ul>
+					</nav>
+
+				</div>
+
+			<!-- Основа -->
+				<div class="wrapper style1">
+
+					<div class="container">
+						<article id="main" class="special">
+							<header>
+								<h2><a href="#">Как это работает?</a></h2>
+								<p>
+									Авторизуйтесь через AWuse, публикуйте записи, ожидайте, получайте ответы и заказы!
+								</p>
+							</header>
+							<a href="#" class="image featured"><img src="/images/pic06.png" alt="" /></a>
+<div class="dv">
+<iframe src="http://award.yzz.me/m/communities/blogs/?id=3" class="ifr">
+</iframe>
+</div>
+							
+						</div>
+
+			<!-- Подвал -->
+		
+
+
+		<!-- Скрипты -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
